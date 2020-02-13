a2ae0b40a0f51bd679685bf2e4d5d27e29493225
Maciej Kozlowski <mjp.kozlowski@gmail.com>
2020-02-13 20:49:11 +0100

    Initial commit

---
+++ src/components/DungeonWalls/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const DungeonWalls = () => {
+  return (
+    <div className="dungeon-walls">
+      <div className="wall__back-leftmost"></div>
+      <div className="wall__back-left"></div>
+      <div className="wall__back-mid"></div>
+      <div className="wall__back-right"></div>
+      <div className="wall__back-rightmost"></div>
+      <div className="wall__mid-left"></div>
+      <div className="wall__mid-center"></div>
+      <div className="wall__mid-right"></div>
+      <div className="wall__front-left"></div>
+      <div className="wall__front-right"></div>
+    </div>
+  );
+};
+
+export default DungeonWalls;
