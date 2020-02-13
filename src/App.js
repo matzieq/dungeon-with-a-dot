d64f36f616b5b661dbadc9e9ee5fa858faf8a8d2
Maciej Kozlowski <mjp.kozlowski@gmail.com>
2020-02-13 21:19:32 +0100

    Add dungeon walls display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import DungeonWalls from "./components/DungeonWalls";
-import "./App.css";
+import "./App.scss";
 
 const App = () => {
+  const walls = [
+    [1, 1, 1, 0, 1],
+    [1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 0]
+  ];
   return (
     <div className="game">
-      <DungeonWalls />
+      <DungeonWalls walls={walls} />
     </div>
   );
 };
--- src/components/DungeonWalls/index.js
+++ src/components/DungeonWalls/index.js
@@ -1,20 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const DungeonWalls = () => {
+const DungeonWalls = ({ walls }) => {
   return (
     <div className="dungeon-walls">
-      <div className="wall__back-leftmost"></div>
-      <div className="wall__back-left"></div>
-      <div className="wall__back-mid"></div>
-      <div className="wall__back-right"></div>
-      <div className="wall__back-rightmost"></div>
-      <div className="wall__mid-left"></div>
-      <div className="wall__mid-center"></div>
-      <div className="wall__mid-right"></div>
-      <div className="wall__front-left"></div>
-      <div className="wall__front-right"></div>
+      {walls[0][0] && (
+        <div className="wall wall__back wall__back--leftmost"></div>
+      )}
+      {walls[0][1] && <div className="wall wall__back wall__back--left"></div>}
+      {walls[0][2] && <div className="wall wall__back wall__back--mid"></div>}
+      {walls[0][3] && <div className="wall wall__back wall__back--right"></div>}
+      {walls[0][4] && (
+        <div className="wall wall__back wall__back--rightmost"></div>
+      )}
+      {walls[1][1] && <div className="wall wall__mid wall__mid--left"></div>}
+      {walls[1][2] && <div className="wall wall__mid wall__mid--center"></div>}
+      {walls[1][3] && <div className="wall wall__mid wall__mid--right"></div>}
+      {walls[2][1] && (
+        <div className="wall wall__front wall__front--left"></div>
+      )}
+      {walls[2][3] && (
+        <div className="wall wall__front wall__front--right"></div>
+      )}
     </div>
   );
 };
 
+DungeonWalls.propTypes = {
+  walls: PropTypes.array.isRequired
+};
 export default DungeonWalls;
