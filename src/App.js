51d967f3df71e134d921e4fe580e0d1f96e58c29
Maciej Kozlowski <mjp.kozlowski@gmail.com>
2020-02-13 21:29:46 +0100

    Import and test proper pixel font

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import "./App.scss";
 
 const App = () => {
   const walls = [
-    [1, 1, 1, 0, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 0, 0]
+    [1, 1, 0, 0, 1],
+    [1, 1, 0, 0, 1],
+    [1, 1, 0, 1, 0]
   ];
   return (
     <div className="game">
       <DungeonWalls walls={walls} />
+      <p style={{ fontSize: "6vh" }}>Something</p>
     </div>
   );
 };
